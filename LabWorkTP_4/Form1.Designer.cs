446e1dc531a0aff2404b5c8d41c654778d0b0403
KuzinMaksim <kuzin-maksim02@yandex.ru>
2021-12-05 14:42:57 +0400

    Добавьте файлы проекта.

---
+++ LabWorkTP_4/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace LabWorkTP_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.EditToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AligmentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.LeftToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.RightToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CenterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.ConfirmToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ToolStripMenuItem
+            // 
+            this.ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.EditToolStripMenuItem,
+            this.ExitToolStripMenuItem});
+            this.ToolStripMenuItem.Name = "ToolStripMenuItem";
+            this.ToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.ToolStripMenuItem.Text = "Файл";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Сохранить";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Открыть";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // EditToolStripMenuItem
+            // 
+            this.EditToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AligmentToolStripMenuItem,
+            this.SizeToolStripMenuItem});
+            this.EditToolStripMenuItem.Name = "EditToolStripMenuItem";
+            this.EditToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.EditToolStripMenuItem.Text = "Верстка";
+            // 
+            // AligmentToolStripMenuItem
+            // 
+            this.AligmentToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.LeftToolStripMenuItem,
+            this.RightToolStripMenuItem,
+            this.CenterToolStripMenuItem});
+            this.AligmentToolStripMenuItem.Name = "AligmentToolStripMenuItem";
+            this.AligmentToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.AligmentToolStripMenuItem.Text = "Выравнивание";
+            // 
+            // LeftToolStripMenuItem
+            // 
+            this.LeftToolStripMenuItem.Name = "LeftToolStripMenuItem";
+            this.LeftToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.LeftToolStripMenuItem.Text = "По левой стороне";
+            this.LeftToolStripMenuItem.Click += new System.EventHandler(this.LeftToolStripMenuItem_Click);
+            // 
+            // RightToolStripMenuItem
+            // 
+            this.RightToolStripMenuItem.Name = "RightToolStripMenuItem";
+            this.RightToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.RightToolStripMenuItem.Text = "По правой стороне";
+            this.RightToolStripMenuItem.Click += new System.EventHandler(this.RightToolStripMenuItem_Click);
+            // 
+            // CenterToolStripMenuItem
+            // 
+            this.CenterToolStripMenuItem.Name = "CenterToolStripMenuItem";
+            this.CenterToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.CenterToolStripMenuItem.Text = "По центру";
+            this.CenterToolStripMenuItem.Click += new System.EventHandler(this.CenterToolStripMenuItem_Click);
+            // 
+            // SizeToolStripMenuItem
+            // 
+            this.SizeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1,
+            this.ConfirmToolStripMenuItem});
+            this.SizeToolStripMenuItem.Name = "SizeToolStripMenuItem";
+            this.SizeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.SizeToolStripMenuItem.Text = "Размер шрифта";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 23);
+            // 
+            // ConfirmToolStripMenuItem
+            // 
+            this.ConfirmToolStripMenuItem.Name = "ConfirmToolStripMenuItem";
+            this.ConfirmToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.ConfirmToolStripMenuItem.Text = "Применить";
+            this.ConfirmToolStripMenuItem.Click += new System.EventHandler(this.ConfirmToolStripMenuItem_Click);
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(12, 37);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(776, 401);
+            this.richTextBox.TabIndex = 1;
+            this.richTextBox.Text = "";
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ExitToolStripMenuItem.Text = "Выход";
+            this.ExitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Text Editor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.ToolStripMenuItem EditToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem AligmentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem LeftToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem RightToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem CenterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripMenuItem ConfirmToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
+    }
+}
+
---
+++ LabWorkTP_4/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabWorkTP_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dgOpenFile = new OpenFileDialog();
+            dgOpenFile.Title = "Загрузка";
+            dgOpenFile.Filter= "RTF Files (*.rtf)|*.rtf";
+            if (dgOpenFile.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox.LoadFile(dgOpenFile.FileName);            
+            }
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dgSaveFile = new SaveFileDialog();
+            dgSaveFile.Title = "Сохраниение";
+            dgSaveFile.DefaultExt = "*.rtf";
+            dgSaveFile.Filter = "RTF Files|*.rtf";
+            if (dgSaveFile.ShowDialog()== DialogResult.OK)
+            {
+                richTextBox.SaveFile(dgSaveFile.FileName.Replace(".txt", ".rtf"), RichTextBoxStreamType.RichText);
+            }
+        }
+
+        private void LeftToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox.SelectionAlignment = HorizontalAlignment.Left;
+        }
+
+        private void RightToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox.SelectionAlignment = HorizontalAlignment.Right;
+        }
+
+        private void CenterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox.SelectionAlignment = HorizontalAlignment.Center;
+        
+        }
+
+        private void ConfirmToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            float i = Convert.ToInt32(toolStripComboBox1.Text);
+            richTextBox.Font = new Font(richTextBox.SelectionFont.FontFamily, i);
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
